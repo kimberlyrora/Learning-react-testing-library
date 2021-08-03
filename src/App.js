447587c83f4ba4d5415783f2e6aa4dd0d19fbe95
krojas <krojas@altimetrik.com>
2021-08-03 15:04:04 -0500

    firsts test about react-testing-library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+	const [btnColor, setBtnColor] = useState("red");
+	const textColor = btnColor === "red" ? "blue" : "red";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<button style={{backgroundColor: btnColor}} onClick={() => {
+			setBtnColor(textColor);
+		}}>Change to {textColor}</button>
     </div>
   );
 }
